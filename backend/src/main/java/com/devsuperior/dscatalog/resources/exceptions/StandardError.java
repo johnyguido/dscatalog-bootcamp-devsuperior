8ac449734c1f2880faed54fbaf9d19aabe894c03
johnyguido <johnyguido@hotmail.com.br>
2021-01-19 22:32:59 -0300

    UPDATE Category

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/StandardError.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/StandardError.java
@@ -3,18 +3,17 @@ package com.devsuperior.dscatalog.resources.exceptions;
 import java.io.Serializable;
 import java.time.Instant;
 
-public class StandardError implements Serializable{
+public class StandardError implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
 	private Instant timestamp;
 	private Integer status;
 	private String error;
 	private String message;
 	private String path;
-	
+
 	public StandardError() {
-		
+
 	}
 
 	public Instant getTimestamp() {
@@ -56,7 +55,5 @@ public class StandardError implements Serializable{
 	public void setPath(String path) {
 		this.path = path;
 	}
-	
-	
 
 }
